f81bc183071435f3893b7bab0a3a387631104e07
lisnju <lis.ebuy@gmail.com>
2013-09-03 00:55:29 +0000

    using i18n

--- web.js
+++ web.js
@@ -4,12 +4,18 @@ var content = fs.readFileSync("index.html").toString();
 var express = require('express');
 var app = express();
 var ejs = require('ejs');
+var i18n = require('i18n');
 
 app.use(express.logger());
+app.use(i18n.init);
 app.set('views',__dirname+'/views');
 app.set('view engine','ejs');
 
+i18n.configure({
+    locales:['en', 'cn'],
+    directory: __dirname + '/locales'
 
+});
 
 var getFileName=function (request){
     var path =__dirname+request.path;
@@ -30,6 +36,21 @@ function pREQ(obj){
 var cn=JSON.parse(getFileContent("data/cn.json"));
 var en=JSON.parse(getFileContent("data/en.json"));
 app.get('/', function(request, response) {
+	response.render("index",en);
+	// response.send(content);
+//  pREQ(request);
+
+});
+
+app.get('/cn', function(request, response) {
+    response.setLocale("cn");
+    response.render("index",cn);
+	// response.send(content);
+//  pREQ(request);
+
+});
+app.get('/en', function(request, response) {
+    response.setLocale("en");
 	response.render("index",en);
 	// response.send(content);
 //  pREQ(request);
